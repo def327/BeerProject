93f9c746bb1c2ac21bddf8d124f4a728e0f38a75
def327 <def3273@gmail.com>
2017-01-13 19:35:16 +0300

    1)Created a sort beer beean in XML document 2)Change fraction didgits to '1' value

--- src/by/beer/entities/beeritem/BeerItem.java
+++ src/by/beer/entities/beeritem/BeerItem.java
@@ -2,5 +2,27 @@ package by.beer.entities.beeritem;
 
 
 public class BeerItem {
+	
+	protected int beerID;
+	protected SortType sortType;
+	protected String manufacturer;
+	protected Alcohol alcohol;
+	
+	//chars
+	
+	protected int alcoholVolume;
+	protected int beerClarity;
+	protected boolean isFiltered;
+	protected double foodValue;
+	
+	//Ingredients
+	
+		protected int water;
+		protected int sugar;
+		protected int hop;
+		protected int malt;
+		protected int yeast;
+	
+	
 
 }
